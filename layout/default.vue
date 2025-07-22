1212000bda78d1027036c4791257d30c1523c5cc
EniV90 <eni_v90@yahoo.com>
2025-07-22 06:34:47 +0100

    first commit

---
+++ layout/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>
+      <ul>
+        <li>
+          <NuxtLink to=" "> Home </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to=" "> Articles </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to=" "> Register </NuxtLink>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <slot>Something should be here</slot>
+    </div>
+
+    <div>
+      <h1>This is a footer</h1>
+    </div>
+  </div>
+</template>
